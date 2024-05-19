56fcbfebf85d2265a41b13d2bb666ccfaa98decc
Fair Lucas <lukitaa.27@gmail.com>
2024-05-19 11:23:15 -0300

    commit 3

--- src/main/java/com/AluraCurso/screenmatch/Repository/SerieRepository.java
+++ src/main/java/com/AluraCurso/screenmatch/Repository/SerieRepository.java
@@ -4,9 +4,27 @@
  */
 package com.AluraCurso.screenmatch.Repository;
 
+import com.AluraCurso.screenmatch.Model.Categoria;
+import com.AluraCurso.screenmatch.Model.Episodio;
 import com.AluraCurso.screenmatch.Model.Serie;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SerieRepository extends JpaRepository<Serie,Long> {
-    
+     Optional<Serie> findByTituloContainsIgnoreCase(String nombreSerie);
+
+    List<Serie> findTop5ByOrderByEvaluacionDesc();
+    List<Serie> findByGenero(Categoria categoria);
+    //List<Serie> findByTotalTemporadasLessThanEqualAndEvaluacionGreaterThanEqual(int totalTemporadas, Double evaluacion);
+
+    @Query("SELECT s FROM Serie s WHERE s.totalTemporadas <= :totalTemporadas AND s.evaluacion >= :evaluacion")
+    List<Serie> seriesPorTemparadaYEvaluacion(int totalTemporadas, Double evaluacion);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:nombreEpisodio%")
+    List<Episodio> episodiosPorNombre(String nombreEpisodio);
+
+    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s = :serie ORDER BY e.evaluacion DESC LIMIT 5 ")
+    List<Episodio> top5Episodios(Serie serie);
 }
